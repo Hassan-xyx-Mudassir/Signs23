a2abb63fe01d448a3b66b63df4dab2faef6e6987
6Hassan-Khan9 <hassankhan7m7@gmail.com>
2025-05-14 02:20:48 +0500

    refactor: remove extra files

--- server/diagnostic_engine/scripts/test.py
+++ server/diagnostic_engine/scripts/test.py
@@ -1,14 +1,7 @@
 from predict_diseases import predict_diseases
 
-
-'''
-All you have to do now is call the function. Pass the gene [BRCA1 | TP53 | PTEN], the path 
-to the patient fasta file, and the path for the output CSV file. Then you can read the output 
-file's last column to get the disease. 
-'''
-
 predict_diseases(
-    gene="PTEN",
-    patient_fasta="../data/samples/patient_pten.fasta",
-    output_csv_path="../data/results/final_predicted_diseases_pten.csv"
+    gene="TP53",
+    patient_fasta="../data/samples/patient_tp53.fasta",
+    output_csv_path="../data/results/final_predicted_diseases_tp53.csv"
 )
